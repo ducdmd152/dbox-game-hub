6cbb48004f8ace4b7c04c64fe9209a4fdcd6564a
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-04-02 11:00:57 +0700

    Showed a spinner while fetch genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,17 @@
-import { HStack, List, ListItem, Text, Image } from "@chakra-ui/react";
+import { HStack, List, ListItem, Text, Image, Spinner } from "@chakra-ui/react";
 import useFetchEntities from "../hooks/useFetchEntities";
 import genreService, { Genre } from "../services/genre-service";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { entities: genres } = useFetchEntities<Genre>(genreService);
+  const {
+    entities: genres,
+    isLoading,
+    error,
+  } = useFetchEntities<Genre>(genreService);
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <List>
       {genres.map((genre) => (
