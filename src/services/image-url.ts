075898e9c02ebfa26138b531cffb3b9945fa56e0
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2023-04-03 08:36:50 +0700

    Handled games with no images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,6 @@
+import noImagePlaceholder from "../assets/no-image-placeholder-6f3882e0.webp";
 const getCroppedImageUrl = (url: string) => {
-  if (!url)
-    return "https://studiolegalelisi.it/wp-content/uploads/2021/03/errore-404.jpg";
+  if (!url) return noImagePlaceholder;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
